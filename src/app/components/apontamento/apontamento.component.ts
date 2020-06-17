dedc8da79d5c130245792a71212c346d68b3add5
jaopol <jaopol@gmail.com>
2020-06-16 22:11:24 -0300

    Corrigindo apresentação do dia

--- src/app/components/apontamento/apontamento.component.ts
+++ src/app/components/apontamento/apontamento.component.ts
@@ -20,7 +20,7 @@ export class ApontamentoComponent implements OnInit {
     const data = new Date();
     // this.apontamento.dataApontamento = data.toDateString();
     this.apontamento.numeroMes = data.getMonth();
-    this.apontamento.diaApontamento = data.getDay();
+    this.apontamento.diaApontamento = data.getDate().valueOf();
     this.apontamento.mesApontamento = Mes[ this.apontamento.numeroMes ];
     this.apontamento.tipoApontamento = TipoApontamento.despesa;
     console.log( this.apontamento.tipoApontamento );
